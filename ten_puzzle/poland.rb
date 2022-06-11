b7c70ad3f32e7d6bed1bf5789885fbb1fb4f59e1
GupiSakaguchi <gupisakaguchi@gmail.com>
2022-06-11 09:26:44 +0900

    [10パズル]逆ポーランド記法を通常の計算式に復元するdecodeを追加

---
+++ ten_puzzle/poland.rb
@@ -0,0 +1,83 @@
+class Poland
+  def calc(exp)
+    space = []
+
+    exp.chars do |char|
+     number?(char) ? space.push(char.to_i) : operator_logic(char, space)
+    end
+
+    space.first
+  end
+
+  def decode(exp)
+    space = []
+
+    exp.chars do |char|
+      if number?(char)
+        space.push(char)
+      else
+        decode_operator_logic(char, space)
+      end
+    end
+
+    space.first
+  end
+
+  private
+
+  def number?(str)
+    # 文字列の先頭(\A)から末尾(\z)までが「0」から「9」の文字か
+    nil != (str =~ /\A[0-9]+\z/)
+  end
+
+  def operator_logic(char, space)
+    second = space.pop
+    first = space.pop
+
+    case char
+    when '+'
+      space.push(first + second)
+    when '-'
+      space.push(first - second)
+    when '*'
+      space.push(first * second)
+    else
+      space.push(first / second)
+    end
+  end
+
+  def decode_operator_logic(char, space)
+    second = space.pop
+    first = space.pop
+
+    case char
+    when '*', '/'
+      if first.size > 1
+        first = "(#{first})"
+      end
+
+      if second.size > 1
+        second = "(#{second})"
+      end
+    end
+
+    case char
+    when '+'
+      space.push("#{first} + #{second}")
+    when '-'
+      space.push("#{first} - #{second}")
+    when '*'
+      space.push("#{first} * #{second}")
+    else
+      space.push("#{first} / #{second}")
+    end
+  end
+end
+
+po = Poland.new
+p po.calc(ARGV[0])
+p po.decode(ARGV[0])
+
+
+
+
