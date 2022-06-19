8cb13158da087535be501903a37b8956f1ef7030
GupiSakaguchi <gupisakaguchi@gmail.com>
2022-06-19 09:00:34 +0900

    [小町算]main処理の雛形実装

--- komachi/komachi.rb
+++ komachi/komachi.rb
@@ -1,2 +1,24 @@
 class Komachi
+  def main
+    vector = []
+
+    rec(vector)
+  end
+
+  private
+
+  def rec(vector)
+    if vector.size == 8
+
+      return
+    end
+
+    5.times do |operator|
+      vector2 = vector # 配列のcopy
+
+      vector2.push(operator)
+
+      rec(vector2)
+    end
+  end
 end
