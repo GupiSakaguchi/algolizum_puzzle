1c62356e95c76aa5519f9d2f24e710e9358ed1e3
GupiSakaguchi <gupisakaguchi@gmail.com>
2022-06-19 09:30:43 +0900

    [小町算]空白連結のロジックを実装

--- komachi/komachi.rb
+++ komachi/komachi.rb
@@ -5,11 +5,11 @@ class Komachi
     rec(vector)
   end
 
-  private
-
+  # 1.空白処理
+  # 2.掛け算・割り算
+  # 3.加減
   def rec(vector)
     if vector.size == 8
-
       return
     end
 
@@ -21,4 +21,41 @@ class Komachi
       vector.pop
     end
   end
+
+  def calc_empty(signs)
+    new_vals = []
+    new_signs = []
+
+    value = 1
+    signs.size.times do |i|
+      add = i + 2
+
+      if signs[i] == Sign::EMPTY
+        value = value * 10 + add
+      else
+        new_vals.push(value)
+        new_signs.push(signs[i])
+
+        value = add
+      end
+    end
+
+    new_vals.push(value)
+    [new_vals, new_signs]
+  end
 end
+
+module Sign
+  EMPTY = 0
+  PLUS = 1
+  MINUS = 2
+  MUL = 3
+  DIV = 4
+end
+
+class Pair
+end
+
+k = Komachi.new
+
+p k.calc_empty([1,0,0,3,4,2,2,0])
