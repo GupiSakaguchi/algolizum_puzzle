0c6bf0a7c8c2f675fce19fab21a344753dd17511
GupiSakaguchi <gupisakaguchi@gmail.com>
2022-08-13 22:24:39 +0900

    リファクタ

--- soudoku/dfs.rb
+++ soudoku/dfs.rb
@@ -4,19 +4,7 @@ def dfs
   start = 0
   treasure = 8
 
-  tree =  [
-    [1],
-    [0, 2, 4],
-    [1, 3],
-    [2],
-    [1, 6],
-    [6, 8],
-    [4, 5, 7],
-    [6, 9],
-    [5],
-    [7]
-  ]
-
+  tree = data
   searched = Array.new(tree.length, false)
 
   searched[start] = true
@@ -34,17 +22,33 @@ def dfs
     node = stack.pop
     searched[node] = true
     msg = "tree[#{node}] を探索 -> "
+
     if node == treasure
       puts msg << "お宝発見！"
-      break
+      return
     else
       puts msg << "お宝はありませんでした"
     end
-    puts "探索済みリスト -> #{searched}"
 
+    puts "探索済みリスト -> #{searched}"
 
     next_nodes = tree[node].select { |node| not searched[node] }
     puts "追加候補 -> #{next_nodes}"
     next_nodes.each { |node| stack.push(node) }
   end
+end
+
+def data
+  [
+    [1],
+    [0, 2, 4],
+    [1, 3],
+    [2],
+    [1, 6],
+    [6, 8],
+    [4, 5, 7],
+    [6, 9],
+    [5],
+    [7]
+  ]
 end
